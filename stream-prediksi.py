bf397938260415f5b7c5921ed9d15c71b0828837
ShifaSilfiana <115074865+ShifaSilfiana18@users.noreply.github.com>
2022-12-08 20:15:05 +0700

    Update stream-prediksi.py

--- stream-prediksi.py
+++ stream-prediksi.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np 
 import streamlit as st
 
-model = pickle.load(open('patient_predictions.sav', 'rb'))
+patient_predictions_model = pickle.load(open('patient_predictions.sav', 'rb'))
 
 st.title('Prediksi Kelangsungan Hidup Pasien Setelah Operasi Payudara')
 
@@ -15,7 +15,7 @@ Axil_nodes = st.text_input('Jumlah kelenjar aksila positif yang terdeteksi')
 patient_diagnosis = ''
 
 if st.button('Prediksi Kelangsungan Hidup Pasien'):
-    patient_prediction = model.predict([[Age, Op_year, Axil_nodes]])
+    patient_prediction = patient_predictionts_model.predict([[Age, Op_year, Axil_nodes]])
     
     if (patient_prediction[0]==1):
         patient_diagnosis = 'Pasien meninggal dalam 5 tahun'
